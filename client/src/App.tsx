325fdf5495571709d6f3035b5b315504bf45aa7c
githubSubhanKhan <subbikhan124@gmail.com>
2025-07-31 17:18:59 +0500

    removed some files and code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
 import { RouterProvider } from "react-router-dom";
-// import { ThemeProvider } from "./contexts/ThemeContext";
 import { router } from "./Router";
 
 export default function App() {
     return (
-        // <ThemeProvider>
             <RouterProvider router={router} />
-        // </ThemeProvider>
     )
 }
